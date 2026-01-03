28692b49feb852052fdffc621cc4637bef01258d
Yuki <yuki.ukyy@gmail.com>
2026-01-03 16:02:56 +0000

    variable support (ability to tokenize)

--- main.cpp
+++ main.cpp
@@ -7,49 +7,40 @@ using std::string;
 
 int error_quit(string msg);
 
-/*
- *
- *      Lexing or tokenizing is converting sets of characters iterated line by line or
- *      character by character into series of tokens which there's a list of it above
- *
- */
+int main(int argc, char *argv[]) {
+  if (argc != 2)
+    return error_quit("Input file to be compiled is missing...");
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-        return error_quit("Input file to be compiled is missing...");
+  string line{};
+  int line_number{0};
+  {
+    std::ifstream input(argv[1], std::ios::binary);
 
-    string line{};
-    int line_number{0};
-    {
-        std::ifstream input(argv[1], std::ios::binary);
+    if (input.fail() || !input.is_open())
+      return error_quit("Unable to open the file");
+    else {
+      std::vector<std::vector<Token>> tokens{};
+      while (getline(input, line)) {
+        if (line.empty())
+          continue;
 
-        if (input.fail() || !input.is_open())
-            return error_quit("Unable to open the file");
-        else
-        {
-            std::vector<Token> tokens{};
-            while (getline(input, line))
-            {
-                if (line.empty())
-                    continue;
+        tokens.push_back(tokenize(line, line_number));
+        line_number++;
+      }
 
-                tokens = tokenize(line, line_number);
-                line_number++;
-            }
-
-            for (Token el : tokens)
-            {
-                std::cout << el.value << "\n";
-            }
+      for (std::vector<Token> el_v : tokens) {
+        for (Token token : el_v) {
+          std::cout << token.value << "\n";
         }
+        std::cout << "\n===================\n\n";
+      }
     }
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
-int error_quit(string msg)
-{
-    std::cerr << msg << std::endl;
-    return EXIT_FAILURE;
+int error_quit(string msg) {
+  std::cerr << msg << std::endl;
+  return EXIT_FAILURE;
 }
--- tokenizer.hpp
+++ tokenizer.hpp
@@ -1,84 +1,113 @@
 #pragma once
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using std::string;
 
-enum class TokenType
-{
-    _return,
-    let,
-    equal,
-    varriable_name,
-    variable_value,
-    int_lit,
-    semi
+/*
+ *
+ *      Lexing/tokenizing, converting sets of characters iterated line by line
+ * or character by character into series of tokens
+ *
+ */
+
+enum class TokenType {
+  _return,
+  let,
+  equal,
+  varriable_name,
+  variable_value,
+  double_quotes,
+  int_lit,
+  semi
 };
 
-struct Token
-{
-    TokenType type;
-    string value;
+struct Token {
+  TokenType type;
+  string value;
 };
 
-std::vector<Token> tokenize(const string &str, const int line_number)
-{
-    std::vector<Token> tokens{};
-    string str_buf{};
+std::vector<Token> tokenize(const string &str, const int line_number) {
+  std::vector<Token> tokens{};
+  string str_buf{};
+  bool isStringStarted{false};
 
-    for (size_t i = 0; i < str.length(); i++)
-    {
-        char c = str.at(i);
+  for (size_t i = 0; i < str.length(); i++) {
+    char c = str.at(i);
 
-        if (std::isalpha(static_cast<unsigned char>(c)) || c == '=')
-        {
-            str_buf.push_back(c);
+    if (std::isalpha(static_cast<unsigned char>(c)) || c == '=' || c == '"' ||
+        std::isdigit(c)) {
+      str_buf.push_back(c);
+    } else if (std::isspace(static_cast<unsigned char>(c))) {
+      if (!str_buf.empty()) {
+        if (str_buf == "let") {
+          tokens.push_back({.type = TokenType::let, .value = "let"});
+        } else if (str_buf == "return") {
+          tokens.push_back({.type = TokenType::_return, .value = "return"});
+        } else if (str_buf == "=") {
+          tokens.push_back({.type = TokenType::equal, .value = "equal"});
+        } else {
+          // std::cout << str_buf << "\n";
+          if (!tokens.empty() && tokens.back().type == TokenType::let) {
+            tokens.push_back(
+                {.type = TokenType::varriable_name, .value = str_buf});
+          } else if (!tokens.empty() &&
+                     tokens.back().type == TokenType::equal) {
+            tokens.push_back({.type = TokenType::variable_value,
+                              .value = str_buf.substr(0, i - 1)});
+          } else {
+            std::cerr << "Incorrect syntax, on line " << line_number << ":" << i
+                      << std::endl;
+            std::exit(EXIT_FAILURE);
+          }
         }
-        else if (std::isspace(static_cast<unsigned char>(c)))
-        {
-            if (!str_buf.empty())
-            {
-                if (str_buf == "let")
-                {
-                    tokens.push_back({.type = TokenType::let, .value = "let"});
-                }
-                else if (str_buf == "return")
-                {
-                    tokens.push_back({.type = TokenType::_return, .value = "return"});
-                }
-                else if (str_buf == "=")
-                {
-                    tokens.push_back({.type = TokenType::equal, .value = "equal"});
-                }
-                else
-                {
-                    if (!tokens.empty() && tokens.back().type == TokenType::let)
-                    {
-                        tokens.push_back({.type = TokenType::varriable_name, .value = str_buf});
-                    }
-                    else if (!tokens.empty() && tokens.back().type == TokenType::equal)
-                    {
-                        tokens.push_back({.type = TokenType::variable_value, .value = str_buf.substr(0, i - 1)});
-                    }
-                    else
-                    {
-                        std::cerr << "Incorrect syntax, on line "
-                                  << line_number << ":" << i << std::endl;
-                        std::exit(EXIT_FAILURE);
-                    }
-                }
 
-                str_buf.clear();
-            }
+        str_buf.clear();
+      }
+    } else if (c == ';') {
+      if (str_buf.at(0) == '"' && str_buf.at(str_buf.length() - 1) == '"') {
+        for (int j = 1; j < str_buf.length() - 1; j++) {
+          char k = str_buf.at(j);
+
+          if (k == '"') {
+            std::cerr << "Incorrect syntax, \"additional \" inside the "
+                         "string\", on line "
+                      << line_number << ":" << i << std::endl;
+            std::exit(EXIT_FAILURE);
+          }
         }
-        if (c == ';')
-        {
-            tokens.push_back({.type = TokenType::variable_value, .value = str_buf});
-            tokens.push_back({.type = TokenType::semi, .value = "semi"});
-            str_buf.clear();
+
+        tokens.push_back({.type = TokenType::variable_value,
+                          .value = str_buf.substr(1, str_buf.length() - 2)});
+      } else {
+        for (int j = 0; j < str_buf.length(); j++) {
+          char k = str_buf.at(j);
+
+          if (!std::isdigit(k)) {
+            std::cerr
+                << "Incorrect syntax, strings need \" at the beginning and "
+                   "ending, otherwise place a number in the variable. on line "
+                << line_number << ":" << i << std::endl;
+            std::exit(EXIT_FAILURE);
+          }
         }
+
+        tokens.push_back({.type = TokenType::variable_value, .value = str_buf});
+      }
+
+      tokens.push_back({.type = TokenType::semi, .value = "semi"});
+      str_buf.clear();
     }
+  }
 
-    return tokens;
+  return tokens;
 }
+
+// std::vector<Token> tokenize(const string &str, const int line_number)
+// {
+//     std::vector<Token> tokens{};
+//     string str_buf{};
+
+//     return tokens;
+// }
